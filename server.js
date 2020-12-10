d258bf4b061897f3e88317f43b7d1c1ea824b54c
Neill Somerville <neill.somerville@gmail.com>
2020-12-09 23:35:37 -0700

    server from scratch

--- server.js
+++ server.js
@@ -1,82 +1,16 @@
-require('dotenv').config();
+const http = require('http');
 
-const path = require('path');
-
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const session = require('express-session');
-const MongoDBStore = require('connect-mongodb-session')(session);
-const csurf = require('csurf');
-const flash = require('connect-flash');
-
-const errorController = require('./controllers/error');
-const User = require('./models/user');
-
-const app = express();
-const store = new MongoDBStore({
-	uri: process.env.MONGODB_URI,
-	collection: 'sessions',
+const server = http.createServer((req, res) => {
+	res.setHeader('Content-Type', 'text/html');
+	res.write(`<html>
+  <head>
+    <title>My first response</title>
+  </head>
+  <h1>Hello friend!</h1>
+  <p>How are you?</p>
+</html>
+`);
+	res.end();
 });
 
-const csrfProtection = csurf();
-
-// const adminRoutes = require('./routes/admin');
-// const shopRoutes = require('./routes/shop');
-// const authRoutes = require('./routes/auth');
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(
-	session({
-		secret: 'my secret',
-		resave: false,
-		saveUninitialized: false,
-		store: store,
-	})
-);
-
-app.use(csrfProtection);
-app.use(flash());
-
-app.use((req, res, next) => {
-	if (!req.session.user) {
-		return next();
-	}
-	User.findById(req.session.user._id)
-		.then(user => {
-			if (!user) {
-				// no user found
-				return next();
-			}
-			req.user = user;
-			next();
-		})
-		.catch(err => {
-			throw new Error(err);
-		});
-});
-
-app.use((req, res, next) => {
-	res.locals.isAuthenticated = req.session.isLoggedIn;
-	res.locals.csrfToken = req.csrfToken();
-	next();
-});
-
-
-app.get('/500', errorController.get500);
-
-app.use(errorController.get404);
-
-app.use((error, req, res, next) => {
-	res.redirect('/500');
-});
-
-mongoose
-	.connect(process.env.MONGODB_URI)
-	.then(result => {
-		app.listen(3000);
-	})
-	.catch(err => {
-		console.log(err);
-	});
+server.listen(3000);
